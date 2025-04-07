4319cadc61396b5f6cf7d0441f9c5447ef424d54
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-04-07 18:01:18 +0530

    Control_statements

---
+++ Html/Control_statements/controls.js
@@ -0,0 +1,72 @@
+//  Control statements
+// if ,if else ,if else lader
+
+
+// IF CONDITION
+
+// SYNTAX:
+
+// if (condition){
+//     statement
+// }
+
+var A= 10
+var B =20
+
+if (A<B){
+    console.log("A is grater than B")
+}
+
+// if else 
+
+// SYNTAX:
+// if (condition){
+//    statement
+// }
+// else{
+//     statement 
+// }
+
+
+
+var A= 10
+var B =20
+
+if (A>B){
+    console.log("A is grater than B")
+}
+else {
+    console.log("A is smaller than B")
+}
+
+var N= 3
+if(N%3==1){
+    console.log("N is even number")
+}
+else{
+    console.log("N is odd number")
+}
+// if else ladder
+
+// SYNTAX:
+// if (condition){
+//    statement
+// }
+// else if{
+//     statement 
+// }
+// else{
+    // statement
+// }
+
+let a=5
+if (a==1){
+    console.log("a value is 1")
+}
+else if(a==5){
+    console.log("a value is 5")
+}
+else {
+    console.log("a is value is not 1 and 5")
+}
+
