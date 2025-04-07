449e4dcca0b33d7de4315dee4c914b440d5d518f
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-04-07 18:03:50 +0530

    Task1_Js

---
+++ Html/Task1/task.js
@@ -0,0 +1,29 @@
+
+// TASK_1(JAVA_SCRIPT)
+
+// Write a JavaScript program that calculates a student’s average grade and prints a message based on the following criteria:
+
+// 90 or above: Print: "Excellent!"
+// 80 to 89: Print: "Good Job!"
+// 70 to 79: Print: "Needs Improvement."
+// 60 to 69: Print: "Passing, but Could Do Better."
+// Below 60: Print: "Failing."
+
+
+let average_markes  = 59
+
+if (average_markes>=90){
+    console.log("Excelent")
+}
+else if (average_markes>=80){
+    console.log("Good job")
+}
+else if (average_markes>=70){
+    console.log("Need imporovement")
+}
+else if (average_markes>=60){
+    console.log("Passing,but Could Do Better")
+}
+else {
+    console.log("Falling")
+}
