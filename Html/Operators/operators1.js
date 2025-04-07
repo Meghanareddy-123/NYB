d14cca4d6f2f1bfba56d66f17642ab1ab9dc97f3
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-04-07 17:57:31 +0530

    operators1

---
+++ Html/Operators/operators1.js
@@ -0,0 +1,48 @@
+
+// comparisition operators , Equality,In equality,Ternary ,logical oparetors
+
+
+// 1.Relational operators < , > , <= , >=
+
+var a1=10
+var a2=10
+console.log(a1<a2)
+console.log(a1>a2)
+
+console.log(a1<=a2)
+console.log(a1>=a2)
+
+// Equality oparetors
+// Types
+// scrict equality (===)
+// loose equality (==)
+
+var b1=10
+var c1=10
+
+console.log(b1==c1)
+console.log(b1===c1)
+
+// InEquality  Operator(!==,!=)
+
+console.log(b1!==c1)
+console.log(b1!=c1)
+
+// Ternary Operator(?:)
+
+console.log(true?console.log("truecase"):console.log("falsecase"))
+
+// console.log(hii?console.log("truecase"):console.log("falsecase")) hii is not defind
+
+console.log(3>2?console.log("truecase"):console.log("falsecase")) 
+
+console.log(1!==1?console.log("truecase"):console.log("falsecase")) 
+
+// Logical Opearetor  (&&,||,!) AND ,OR, Not 
+
+let a12 = 10
+let b12= 10
+console.log(a12==b12 && a12>=b12)
+
+console.log(a12!==b12 || a12>=b12)
+
