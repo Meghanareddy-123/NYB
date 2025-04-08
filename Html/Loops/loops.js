39057ea9cae631b334fbfd4ca8c97d4f9e112d95
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-04-08 20:24:36 +0530

    Loops

---
+++ Html/Loops/loops.js
@@ -0,0 +1,60 @@
+// LOOPS
+
+// CONDITIONAL LOOPS 
+// i.for loop 
+// ii. while loop
+// iii. do while loop
+// iv. Infinity loop
+
+
+// for loop
+// syntax:
+// for(dlaclaration ;condition ; updation){
+
+// }
+
+// let A = 10
+ for(i=0; i<5 ; i++){     
+    console.log(i)
+}
+
+// for(a=2; a<10 ; a++){
+    // console.log(a)
+// }
+
+//  for(i=1; i<10 ; i++){
+    // console.log(a*2)
+//  }
+
+// for(a=2; a<10 ; a++){
+//     console.log(a-1)
+// }
+
+// while loop
+// syntax:
+// declaration
+// while(condition){
+//     statement 
+//     updation
+// }
+// let a=10
+// while (a<10){
+    // console.log(a)
+    // a++
+// }
+
+// do while
+
+// do{
+//     statement
+//     updatiton
+// }
+// while(condition)
+
+var a1= 10
+do{
+   console.log(a1)
+    a1++
+}
+while (a1<10)
+
