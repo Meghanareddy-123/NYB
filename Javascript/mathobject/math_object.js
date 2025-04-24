8f232979a3758d5a18fbd82876b1336aa5f5c787
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-04-24 16:01:14 +0530

    Math_object.js

---
+++ Javascript/mathobject/math_object.js
@@ -0,0 +1,36 @@
+// Math object
+// min,max,power,ceel,floor,round,squrt,absoulte
+
+console.log(Math.min(1,20,30,56,79,90,68,90))
+console.log(Math.max(1,20,30,56,79,90,68,90))
+ console.log(Math.pow(2,3))  
+// console.log(Math.mod(10,2))
+ console.log(Math.floor(2.6))
+console.log(Math.ceil(3.6))
+console.log(Math.sqrt(25))
+console.log(Math.abs(-2.6))
+
+// DATE OBJECT
+
+var d=new Date().getDate()
+console.log(d)
+
+var d1=new Date().getFullYear()
+console.log(d1)
+var d2=new Date().getMonth()
+console.log(d2)
+var d3=new Date().getTime()
+console.log(d3)
+var d7=new Date().getDay()
+console.log(d7)
+
+var d4=new Date().getHours()
+console.log(d4)
+var d5=new Date().getSeconds()
+console.log(d5)
+var d6=new Date().getTimezoneOffset()
+console.log(d6)
+
+
+var c1 = new Date().setDate()
+// console.log(c1.)
