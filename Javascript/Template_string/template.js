158eb18f7240083c72d2edcd9c9af0e5a39f0e75
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-04-24 21:15:41 +0530

    Template_string.js

---
+++ Javascript/Template_string/template.js
@@ -0,0 +1,68 @@
+// Template string
+
+var a=10
+var b= 20
+console.log("sum of a and b",a+b)
+
+// Template string
+// Embedding value and string
+
+console.log(`sum of ${a} and ${b}:${a+b} `)
+
+var a=10
+var b= 20
+console.log("sum of a and b",a*b)
+
+console.log(`sum of ${a} and ${b}:${a*b} `)
+
+// STRING
+// collection of Character
+
+// string methods
+// 1.length of the string
+
+var b='meghana'
+console.log(b)
+console.log(b.length)
+
+var string =" this is strig object"
+console.log(string.length)
+
+// Remove white space
+
+// there are 3 ways
+// 1.trim():it remove white spaces both sides
+// 2.trimStart():it remove only begining of the string
+// 3.trimEnd():it remove only ending of the string
+
+var a= "   megha   "
+console.log(a)
+console.log(a.trim())
+console.log(a.trimStart())
+console.log(a.trimEnd())
+
+// Extraction a specific part of the string
+
+var a="hello good morning" 
+
+console.log(a.slice(0,4))
+console.log(a.slice(4,10))
+console.log(a.slice(11,a.length))
+console.log(a.slice(0,-1))
+
+// substring
+
+console.log(a.substring(0,a.length))
+console.log(a.substring(0,-1))
+
+// substr(substr(starting,length))
+console.log(a.substr(2,10))
+console.log(a.substr(0,12))
+
+//n
+console.log(a.indexOf("n"))
+console.log(a.indexOf("o"))
+
+// ChatAT
+// STINGNAME.CharAt(indexvalue)
+console.log(a.ChatAt(0))
