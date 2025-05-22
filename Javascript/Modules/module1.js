6b8c1726e320d6cd50eba81e0c9678eb0424ec68
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-05-22 11:44:26 +0530

    Modules.js

---
+++ Javascript/Modules/module1.js
@@ -0,0 +1,11 @@
+
+
+// import from ''
+// export default function login(){
+    // console.log("logged into your account")
+// }
+
+export function add(a,b){
+    console.log(a,b)
+        // return a+b
+}
---
+++ Javascript/Modules/module2.js
@@ -0,0 +1,13 @@
+
+  export function singup(){
+    console.log("sinup")
+
+  }
+
+ 
+
+// export default function add(a,b){
+//     console.log(a,b)
+//     return a+b
+// }
+export let a=10;
