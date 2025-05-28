8f1451ab3eec003f4da8529a820a1b2f4d47b27a
MEGHANA <meghana.n@navayuvabharatinfotech.com>
2025-05-28 18:10:39 +0530

    Js_output_method

---
+++ Javascript/Js_output_method/output.js
@@ -0,0 +1,21 @@
+
+// JS OUTPUT METHODS
+
+// console.log("hello")
+
+document.write("hii")
+
+// alert: it display alert message 
+
+alert("how are you")
+
+// prompt: to collect the data from the user
+
+// prompt("enter your name")
+
+var a= prompt("enter your name")
+console.log(a)
+
+// confirm : it return true or false
+var b=confirm("are you sure")
+console.log(b)
